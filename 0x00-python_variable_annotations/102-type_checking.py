3151cb6f7cee0f3822b27cff0100fa1ef2756d0b
== <==>
2023-12-07 19:30:36 +0000

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """Type Checking"""
-from typing import Tuple, List
+from typing import Tuple, List, Union
 
-def zoom_array(lst: Tuple[int, ...], factor: float = 2) -> List[int]:
+def zoom_array(lst: List[int], factor: Union[int, float] = 2) -> List[int]:
     """
     Refactor Types to get output: 
     {'lst': typing.Tuple, 'factor': <class 'int'>, 'return': typing.List}
     """
-    zoomed_in: List[int] = [int(i * factor) for i in lst]
+    zoomed_in: List[int] = [
+        item for item in lst
+        for _ in range(int(factor))
+        ]
     return zoomed_in
 
 
