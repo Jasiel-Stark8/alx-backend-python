2cc98acbc526267b6db2f0422716c32a9272cd57
== <==>
2023-12-07 16:53:09 +0000

    Complex types - functions

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,10 +6,5 @@ from typing import Callable
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return multiplied arg as float"""
     if multiplier is None:
-        return lambda x: x
-
-    def multiplier_function(x: float) -> float:
-        """Finally return x * multiplier"""
-        return x * multiplier
-
-    return multiplier_function
+        raise ValueError('Multiplier must be a float')
+    return lambda x: x * multiplier
