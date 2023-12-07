ba8264b629d53f1064d88ebc52ea31e8d4e53e1d
== <==>
2023-12-07 18:12:42 +0000

    More involved type annotations | modular type

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """More involved type annotations"""
-from typing import Mapping, Any, Optional, TypeVar
+from typing import Mapping, Any, Optional, TypeVar, Union
 
 
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping[Any, T], key: Any, default: Optional[T] = None) -> Optional[T]:
+def safely_get_value(dct: Mapping[Any, T], key: Any, default: Optional[T] = None) -> Union[Any, T]:
     """Add type annotations to function"""
     if key in dct:
         return dct[key]
