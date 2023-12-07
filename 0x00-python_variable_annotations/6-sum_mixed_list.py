6215881ef0efaa3bc1bb3a74c6b7e15f77b5ee0b
== <==>
2023-12-07 16:19:03 +0000

    Complex types - list of floats

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """Complex types - mixed list"""
 from typing import Union, List
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Return sum of intergers in a mixed list as float"""
     if mxd_lst is None:
