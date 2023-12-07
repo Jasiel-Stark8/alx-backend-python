ca20fd37963bd7d24b398314ff3c1d453299e7b3
== <==>
2023-12-07 18:19:13 +0000

    More involved type annotations | modular type

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""More involved type annotations"""
-from typing import Mapping, Any, Optional, TypeVar, Union
+"""Contains method that safely gets value from dictionary."""
+from typing import Mapping, Any, Union, TypeVar, Optional
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Optional[T] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> Union[Any, T]:
     """Safely gets value from dictionary.
     Args:
         dct (dict): Dictionary to get value from.
