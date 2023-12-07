4a3f2acd2fa5de710f513e58aa10dc5f7ebcba79
== <==>
2023-12-07 16:05:48 +0000

    Complex types - list of floats

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Complex types - list of floats"""
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Return sum of list as float"""
-    total: float = 0.0
-    for i in input_list:
-        total += i
-    return total
+    if input_list is None:
+        return 0
+    return sum(input_list)
