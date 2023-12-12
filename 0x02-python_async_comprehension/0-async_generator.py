4e7a72969d0d6501a8fac99dbe51c15430238e65
== <==>
2023-12-12 23:28:28 +0000

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,19 +3,21 @@
 
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[int, None]:
-    for i in random.randint(range(10)):
+async def async_generator() -> Generator[float, None, None]:
+    """Yield a random integer after every 1 seconds of sleep within a range"""
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield i
-
-
-async def main():
-    async for value in async_generator():
-        print(value)
+        yield random.uniform(0, 10)
 
 
 if __name__ == '__main__':
+    async def main():
+        result = []
+        async for value in async_generator():
+            result.append(value)
+        print(result)
+
     asyncio.run(main())
