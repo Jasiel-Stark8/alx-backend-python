4246090ef7a0ba1df40d1e82808b04d17082b33a
== <==>
2023-12-12 01:04:30 +0000

    Async Basics

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,11 +6,11 @@ import random
 
 
 async def wait_random(max_delay=10):
-    delay = await random.randint(0, max_delay)
+    """Asynchronous coroutine waits for a random delay"""
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    return float(delay)
+    return delay
 
 
 if __name__ == '__main__':
-    result = asyncio.run(wait_random())
-    print(f'{result}')
+    asyncio.run(wait_random())
