06e6403ea08877f15f42e287d86bd39fdb0cc5c7
== <==>
2023-12-12 19:18:31 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[int, None]:
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield i
+
+
+async def main():
+    async for value in async_generator():
+        print(value)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
