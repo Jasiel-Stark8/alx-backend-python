a337b2d909fe1721fb288df827cf6313907ca227
== <==>
2023-12-12 19:26:03 +0000

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Async Generator"""
 
+import random
 import asyncio
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[int, None]:
-    for i in range(10):
+    for i in random.randint(range(10)):
         await asyncio.sleep(1)
         yield i
 
