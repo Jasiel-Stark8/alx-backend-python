6a53bf045f7740607b2c4d2eff4863134d864b1e
== <==>
2023-12-12 10:57:31 +0000

    concorent routines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-"""Contains a method that spawns wait_random n times with a
-specified delay between each call."""
-import asyncio
-from typing import List
+"""execute multiple coroutines at the same time with async"""
 
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with a specified delay
-    between each call.
+async def wait_n(n, max_delay) -> list[float]:
+    """_summary_
+
     Args:
-        n: number of times to spawn wait_random
-        max_delay: maximum delay between each call
+        n (_type_): _description_
+        max_delay (_type_): _description_
+
     Returns:
-        list of delays
+        list[float]: _description_
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    spawn  = [wait_random(max_delay) for _ in range(n)]
+    completed = await asyncio.gather(*spawn)
+    return completed
