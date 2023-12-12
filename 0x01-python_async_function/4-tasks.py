3b05021a0c2dbfd5fa9bbb5c09a13efa8dc8372a
== <==>
2023-12-12 14:47:25 +0000

    Refactor codebase 1 to accept task_wait_random

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+"""execute multiple coroutines at the same time with async"""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, task_wait_random: int) -> List[float]:
+    """
+    Executes multiple coroutines concurrently and returns a list of their results.
+
+    Args:
+        n (int): The number of coroutines to execute.
+        max_delay (int): The maximum delay to pass to the wait_random coroutine.
+
+    Returns:
+        List[float]: A list of the delays (floats) from each completed coroutine.
+    """
+    tasks  = [asyncio.create_task(wait_random(task_wait_random)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
