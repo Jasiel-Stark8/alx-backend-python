425beb68e6e6c23b208102d5b43faab9db1368d2
== <==>
2023-12-13 00:03:19 +0000

    Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""Async Comprehensions"""
+
+import asyncio
+import random
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Yield Async Comprehension"""
+    return [i async for i in async_generator()]
+
+
+if __name__ == '__main__':
+    async def main():
+        await i in async_comprehension()
+        print(results)
+
+    asyncio.run(main())
