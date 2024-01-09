22180ce595f6bba92883dd0b2d840651c3a3217c
iresville <iresgh1@gmail.com>
2024-01-09 03:50:28 +0000

    test utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,27 +4,27 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """Test Class for access_nested_map"""
-
     @parameterized.expand([
-        ({"a": {"b": {"c": 1}}}, ["a", "b", "c"], 1),
-        ({"x": {"y": {"z": 2}}}, ["x", "y", "z"], 2),
-        # Add more test cases here
+        ({'a': {'b': {'c': 1}}}, ['a', 'b', 'c']),
+        ({'x': {'y': {'z': 2}}}, ['x', 'y', 'z'])
     ])
-    def test_access_nested_map_valid(self, nested_map, path, expected):
+    def test_access_nested_map_valid_input(self, nested_map, path):
         """Test access_nested_map with valid input"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        self.assertEqual(access_nested_map(nested_map, path))
 
     @parameterized.expand([
-        ({"a": {"b": {"c": 1}}}, ["a", "b", "d"]),
-        ({"x": {}}, ["x", "y"]),
-        # Add more test cases for invalid input here
+        ({'a': {'b': {'c': 1}}}, ['a', 'b', 'c']),
+        ({'x': {'y': {}}}, ['x', 'y', 'z']),
+        ({'j': {}}, ['j', 'k'])
     ])
-    def test_access_nested_map_invalid(self, nested_map, path):
+    def test_access_nested_map_invalid_input(self, nested_map, path):
         """Test access_nested_map with invalid input"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
 
 if __name__ == "__main__":
     unittest.main()
